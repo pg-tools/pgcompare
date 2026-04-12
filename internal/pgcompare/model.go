0ad1be818c6bca1d13974425e2e86e3a4599e4c5
ssokov <sokovykh.work@gmail.com>
2026-04-12 14:20:56 +0300

    Wire iterations and concurrency into report header

--- internal/pgcompare/model.go
+++ internal/pgcompare/model.go
@@ -39,6 +39,8 @@ type BenchResult struct {
 
 type ReportData struct {
 	GeneratedAt time.Time
+	Iterations  int
+	Concurrency int
 	Before      *BenchResult
 	After       *BenchResult
 	Diffs       []PlanDiff
